ceba471b0e4ac6a412894088f0293289a3134d96
Jacob Turner <44938917+Xychic@users.noreply.github.com>
2020-01-27 19:12:55 +0000

    Added support for image links

--- boostread.py
+++ boostread.py
@@ -1,4 +1,8 @@
 import os
+from shutil import copyfile
+
+# Replace with actual user name and repo name
+githubLink = "https://github.com/{githubName}/{repoName}/raw/master"
 
 try:
     os.mkdir("./markdown")
@@ -18,7 +22,7 @@ for root, dirs, files in os.walk("./notes"):
         with open("./notes/" + filename,"r") as file:
             for line in file:
                 if "title: " in line:
-                    outputName = line[8:-2].replace(" ","_")
+                    outputName = line[8:-2].replace(" ", "_")
                 elif "tags" in line and "]" not in line:
                     folderName  += (file.readline().strip()[1:-1] + "/").replace(" ","_")
                     try:
@@ -34,4 +38,19 @@ for root, dirs, files in os.walk("./notes"):
                         output.close()
                     write = False
                 if write:
-                    output.write(line[2:])
+                    if ":storage/" in line:
+                        try:
+                            os.mkdir("./markdown/src")
+                        except:
+                            pass
+                        i = 0
+                        j = len(line)-1
+                        while line[i] != "/":  i+=1
+                        while line[j] != "/":   j-=1
+                        src = "./attachments/" + line[i+1:-2]
+                        dst = "./markdown/src" + line[j:-2]
+                        copyfile(src, dst)
+
+                        output.write(line[2:i-8] + githubLink + "/src/" + line[j:-2] + ")\n")
+                    else:
+                        output.write(line[2:])
