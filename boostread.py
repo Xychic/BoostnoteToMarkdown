d7f6aa9f43bd9280a8b79256c58e3c08a1e9c7c8
Jacob Turner <44938917+Xychic@users.noreply.github.com>
2020-01-30 19:10:27 +0000

    Comments

--- boostread.py
+++ boostread.py
@@ -8,22 +8,26 @@ PUSH_TO_GIT_PROMPT = True
 AUTO_GENERATE_README = True
 DEFAULT_PATH = str(Path.home()) + "/Documents"
 
+# Creates a parser for system arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dir", default=None, help="The directory where the program will run from the default path (currently \"{0}\")".format(DEFAULT_PATH))
 parser.add_argument("-f", "--fdir", default=None, help="The full directory to where the program will run")
 parser.add_argument("-q", "--quiet", action="store_true", help="Will not prompt if no tags are given or files are one line long.")
 args = parser.parse_args()
 
-# User to get the path of the python file itself and not where it is being executed from
+# The full directory argument takes priority
 if args.fdir is not None:
     directory = args.fdir
+# Then the "From default directory" tag
 elif args.dir is not None:
     directory = DEFAULT_PATH + args.dir
+# If no directory tags are given, use the path for the program
 else:
     directory = sys.path[0]
 
 # Default dict for storing the names of the folders and items
 filesInRepo = defaultdict(list)
+# Default tag is empty
 tag = ""
 
 # Creates the directory if it doesn't already exist
@@ -49,6 +53,7 @@ for root, dirs, files in os.walk(directory + "/notes"):
         # Open the file and interate over the lines
         with open(directory + "/notes/" + filename,"r") as file:
             for line in file:
+                # Remove weird new line stuff
                 line = line.strip()
 
                 # Get the outputname from the title and replace spaces with underscores
@@ -61,10 +66,14 @@ for root, dirs, files in os.walk(directory + "/notes"):
                                     
                 # If the line starts with 'content: ' then the next block is what we need to write
                 elif "content: '''" in line:
+                    # If the tag has not been set and the quiet flag has not been set, ask for a flag
                     if tag == "" and not args.quiet:
-                        tagQuery = input("File: \"{0}\" has no tag.\nDo you want to add a tag? [y/n]: ".format(outputName))
+                        # Ask the user if they want to add a flag
+                        tagQuery = input("File: \"{0}\" has no tag.\nDo you want to add a tag? [y/N]: ".format(outputName))
                         if tagQuery.lower() in ["y","yes"]:
+                            # Get the tag from the user
                             tag = input("Enter a tag: ") +  "/"    
+                    # Appenf the tag to the folder name
                     folderName += tag    
 
                     # Create the subfolder if it doesn't exist
@@ -74,6 +83,8 @@ for root, dirs, files in os.walk(directory + "/notes"):
 
                     # Create a file and open it
                     output = open(folderName + outputName + ".md","w")
+
+                    # Add the file to the directory listing
                     if folderName != directory + "/markdown/":
                         filesInRepo[folderName[len(directory + "/markdown/"):-1]].append(outputName)
                     else:
@@ -121,26 +132,25 @@ for root, dirs, files in os.walk(directory + "/notes"):
                     # If there are no files embeded on the line, just write it to the output
                     else:   output.write(line + "  \n")
 
+# Check to see if the readme flag is true
+if AUTO_GENERATE_README:
+    # Create the readme file
+    with open(directory + "/markdown/README.md","w") as README:
+        # Write the title
+        README.write("# Folders  \n")
+        # Iterate over all the folders and items that will be uploaded
+        for folder, items in filesInRepo.items():
+            # Write the subheading
+            README.write("  \n## {0}  \n".format(folder))
+            # Write the filenames as links
+            for name in sorted(items):
+                README.write("- [{0}](./{1}/{0})  \n".format(name, folder) if folder != "Other_Files" else "- [{0}](./{0})  \n".format(name))
+
 # If the user wants to push to git
 if PUSH_TO_GIT_PROMPT:
     # Check the user actually wants to push to git
-    pushQuery = input("Do you want to push to github repo? [y/n]: ")
+    pushQuery = input("Do you want to push to github repo? [y/N]: ")
     if pushQuery.lower() in ["y","yes"]:
-
-        # Check to see if the readme flag is true
-        if AUTO_GENERATE_README:
-            # Create the readme file
-            with open(directory + "/markdown/README.md","w") as README:
-                # Write the title
-                README.write("# Folders  \n")
-                # Iterate over all the folders and items that will be uploaded
-                for folder, items in filesInRepo.items():
-                    # Write the subheading
-                    README.write("  \n## {0}  \n".format(folder))
-                    # Write the filenames as links
-                    for name in sorted(items):
-                        README.write("- [{0}](./{1}/{0})  \n".format(name, folder) if folder != "Other_Files" else "- [{0}](./{0})  \n".format(name))
-
         # Get a commit message from the user
         message = input("Enter a commit message: ")
         # cd into the directory, add all the files, commit with the specified message, and push
